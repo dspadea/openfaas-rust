e9fb451fbf67f22fa0fdaae4d21792023be3755b
Dave Spadea <david.spadea@gmail.com>
2022-01-11 13:30:16 -0500

    Better template showing send/receive of arbitrary JSON object

--- template/rust/function/src/lib.rs
+++ template/rust/function/src/lib.rs
@@ -1,3 +1,19 @@
-pub fn handle(req : String) -> String {
-    req
+use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
+
+#[derive(Deserialize, Debug)]
+pub struct RequestObject {
+    name: String
+}
+
+#[derive(Serialize, Debug)]
+pub struct ResponseObject {
+    greeting: String
+}
+
+pub async fn handle(req: RequestObject) -> ResponseObject {
+
+    ResponseObject {
+        greeting: format!("Hello, {}", req.name)
+    }
 }
