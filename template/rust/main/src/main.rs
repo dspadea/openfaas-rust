074112cae36df1ba4f1806fcbfbaf53267e1bf6d
Dave Spadea <david.spadea@gmail.com>
2022-01-12 05:54:40 -0500

    Roll back unwind catching and simplify main. Watchdog catches stderr and winds up messing up the output anyway. Bigger problem to solve.

--- template/rust/main/src/main.rs
+++ template/rust/main/src/main.rs
@@ -1,65 +1,20 @@
+use std::io;
+
 extern crate handler;
 
-use std::io;
-use std::panic;
+#[tokio::main]
+async fn main() -> io::Result<()> {
 
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug)]
-struct ErrorMessage {
-    error: ErrorValue,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct ErrorValue {
-    message: String,
-}
-
-fn main() -> io::Result<()> {
     let stdin = io::stdin();
     let handle = stdin.lock();
 
-    let req = match serde_json::from_reader(handle) {
-        Ok(r) => r,
-        Err(e) => {
-            print!("{}",
-                   serde_json::to_string(&ErrorMessage {
-                       error: ErrorValue {
-                           message: format!("{:#?}", e)
-                       }
-                   }).unwrap());
-            return Ok(());
-        }
-    };
+    let req = serde_json::from_reader(handle).expect("Deserializing input");
 
-    let block_result = panic::catch_unwind(move || {
-        let rt = tokio::runtime::Builder::new_multi_thread()
-            .enable_all()
-            .build()
-            .unwrap();
+    let response_obj = handler::handle(req).await;
 
-        return rt.block_on(async {
-            handler::handle(req).await
-        });
-    });
+    let response = serde_json::to_string(&response_obj).unwrap();
 
-    let result = match block_result {
-        Ok(response_obj) => {
-            match response_obj {
-                Ok(r) => serde_json::to_string(&r).unwrap(),
-                Err(e) => serde_json::to_string(&e).unwrap(),
-            }
-        }
-        Err(e) => {
-            serde_json::to_string(&ErrorMessage {
-                error: ErrorValue {
-                    message: format!("{:#?}", e)
-                }
-            }).unwrap()
-        }
-    };
-
-    println!("{}", result);
+    println!("{}", response);
 
     Ok(())
 }
